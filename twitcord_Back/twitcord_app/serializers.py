1fe58487deb174b8fbf4837496ee8a2a0f10aeb1
mohammad <gamerplus7831@yahoo.com>
2021-03-15 15:32:41 +0330

    Add custromdetailuser class in serializers.py

--- twitcord_Back/twitcord_app/serializers.py
+++ twitcord_Back/twitcord_app/serializers.py
@@ -10,3 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = twitcordUser
         fields = 'email'
+
+
+class CustomUserDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = twitcordUser
+        fields = ('email', 'pk', 'profile_img')
+        read_only_fields = ('email',)
