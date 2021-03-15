259f865b10164e79ffa8c7bf9643568d86d1347e
mohammad <gamerplus7831@yahoo.com>
2021-03-15 15:25:24 +0330

    Create serializers.py file and add userserializer

---
+++ twitcord_Back/twitcord_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import NotFound
+
+from .models import *
+from .models import BegardUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BegardUser
+        fields = 'email'
