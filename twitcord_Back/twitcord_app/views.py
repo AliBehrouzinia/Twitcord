90b27e52551cc8e238413fd7709428fe667cf9de
eatmore18 <reza_alidoost@hotmail.com>
2021-03-17 18:23:50 +0330

    Add the post request for creating tweets

--- twitcord_Back/twitcord_app/views.py
+++ twitcord_Back/twitcord_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from . import models, serializers
+from django.http import HttpResponseBadRequest
+from rest_framework.response import Response
+import datetime
 
 # Create your views here.
+
+class TweetsView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated,]
+
+    def post(self, request, *args, **kwargs):
+        data = self.request.data
+        if not data.get('content'):
+            return HttpResponseBadRequest("content is required.")
+        tweet = self.create_tweet(data)
+        return Response( data = serializers.TweetSerializer(tweet).data, status=status.HTTP_200_OK)
+    
+    def create_tweet(self,data):
+        data['create_date'] = datetime.datetime.now()
+        data['user'] = self.request.user.id
+        serializer = serializers.TweetSerializer(data= data, many=False)
+        if serializer.is_valid(True):
+            tweet = serializer.save()
+            return tweet
+        return None
