56c79ef1e12d7ddbe6d4b66c7a565093a0e36bcb
mohammad <gamerplus7831@yahoo.com>
2021-03-31 21:32:56 +0430

    Add ProfileDetailView class in views.py to showing userinfo in profile

--- twitcord_Back/twitcord_app/views.py
+++ twitcord_Back/twitcord_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.http import JsonResponse
+from . import models, serializers
+from django.http import HttpResponseBadRequest
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+import datetime
+
 
 # Create your views here.
+
+class ProfileDetailsView(generics.RetrieveAPIView):
+    """Get profile details of a user"""
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        target_user = get_object_or_404(models.TwitcordUser, id=self.kwargs.get('id'))
+        data = dict()
+        data['username'] = target_user.email
+        data['profile_image'] = target_user.profile_img.url
+        data['date_joined'] = target_user.date_joined
+        data['is_public'] = target_user.is_public
+        data['is_active'] = target_user.is_active
+        data['tweets_count'] = 0
+
+        return Response(data=data, status=status.HTTP_200_OK)
