0921ae70641b414f142b18301ee3a5be2713693a
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-06 22:49:32 +0430

    Add redux to tweetBox to save tweet text

--- twitcord_front/src/components/TweetBox/TweetBox.js
+++ twitcord_front/src/components/TweetBox/TweetBox.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import '../TweetBox/TweetBox.css';
+import * as actionTypes from '../../redux/actionTypes';
+import {connect} from 'react-redux';
+import './TweetBox.css';
+import PropTypes from 'prop-types';
 
 const TweetBox = (props) => (
   <div className="tweet-box">
@@ -9,6 +12,7 @@ const TweetBox = (props) => (
       rowsMin={4}
       rowsMax={4}
       placeholder="what is in your mind?"
+      onChange={(e) => props.onTweetTextChanged(e.target.value)}
     />
     <div className="bottom-bar">
       <Button className="cancel tweet-box-button" variant="contained">
@@ -25,4 +29,17 @@ const TweetBox = (props) => (
   </div>
 );
 
-export default TweetBox;
+TweetBox.propTypes = {
+  onTweetTextChanged: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTweetTextChanged: (tweetText) => {
+      console.log(tweetText);
+      dispatch({type: actionTypes.SET_TWEET_TEXT, tweetText: tweetText});
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(TweetBox);
