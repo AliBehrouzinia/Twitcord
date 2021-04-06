9ccc9a3e42e97a56351270dd02d37311c68c7a57
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-06 23:16:54 +0430

    Make tweetBox component responsive with Grid

--- twitcord_front/src/Components/TweetBox/TweetBox.js
+++ twitcord_front/src/Components/TweetBox/TweetBox.js
@@ -2,31 +2,40 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import * as actionTypes from '../../redux/actionTypes';
+import Grid from '@material-ui/core/Grid';
 import {connect} from 'react-redux';
 import './TweetBox.css';
 import PropTypes from 'prop-types';
 
 const TweetBox = (props) => (
-  <div className="tweet-box">
-    <TextareaAutosize
-      rowsMin={4}
-      rowsMax={4}
-      placeholder="what is in your mind?"
-      onChange={(e) => props.onTweetTextChanged(e.target.value)}
-    />
-    <div className="bottom-bar">
-      <Button className="cancel tweet-box-button" variant="contained">
-        cancel
-      </Button>
-      <Button
-        className="submit tweet-box-button"
-        variant="contained"
-        color="primary"
-      >
-        post
-      </Button>
-    </div>
-  </div>
+  <Grid container className="tweet-box">
+    <Grid item xs={12}>
+      <TextareaAutosize
+        rowsMin={4}
+        rowsMax={4}
+        placeholder="what is in your mind?"
+        onChange={(e) => props.onTweetTextChanged(e.target.value)}
+      />
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container className="bottom-bar">
+        <Grid item xs={12} sm={3} md={2}>
+          <Button className="cancel tweet-box-button" variant="contained">
+            cancel
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={3} md={2}>
+          <Button
+            className="submit tweet-box-button"
+            variant="contained"
+            color="primary"
+          >
+            post
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  </Grid>
 );
 
 TweetBox.propTypes = {
