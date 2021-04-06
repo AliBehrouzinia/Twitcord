eca851c5b702ccd804d53d1755416e1ac051ac3c
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-06 22:53:21 +0430

    Rename components package to Components

--- twitcord_front/src/redux/Reducers/TweetReducer.js
+++ twitcord_front/src/redux/Reducers/TweetReducer.js
@@ -1,14 +1,12 @@
 import {SET_TWEET_TEXT} from '../actionTypes';
 
 const initialState = {
-  tweetText: 'SS',
+  tweetText: null,
 };
 
 const tweetReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_TWEET_TEXT:
-      console.log(state);
-      console.log(action);
       return {...state, tweetText: action.tweetText};
     default:
       return state;
