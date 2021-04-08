50bb35b4d24a1448764171d1c3839eb8df591ddc
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-08 19:58:01 +0430

    Add tweetCharCount to tweetReducer state

--- twitcord_front/src/redux/Reducers/TweetReducer.js
+++ twitcord_front/src/redux/Reducers/TweetReducer.js
@@ -1,13 +1,19 @@
 import {SET_TWEET_TEXT} from '../actionTypes';
 
 const initialState = {
-  tweetText: null,
+  tweetText: '',
+  tweetCharCount: 0,
 };
 
 const tweetReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_TWEET_TEXT:
-      return {...state, tweetText: action.tweetText};
+      return {
+        ...state,
+        tweetText: action.tweetText,
+        tweetCharCount:
+          action.tweetText != undefined ? action.tweetText.length : 0,
+      };
     default:
       return state;
   }
