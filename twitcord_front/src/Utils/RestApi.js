ef52df77088605cbe8d5de4212c428e27f7f92c7
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-12 23:29:15 +0430

    Init axios in RestApi

---
+++ twitcord_front/src/Utils/RestApi.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import axios from "axios";
+import * as Constants from "./Constants";
+
+const instance = axios.create({
+  baseURL: Constants.BASE_URL,
+  responseType: "json",
+  headers: {
+    Authorization: "token 3c2380f638b1a5456efe4621a5ff3debac83e457",
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
