5266f26ba3f2faf30c96c655f3eb024e469aad10
mohammad <gamerplus7831@yahoo.com>
2021-04-12 19:57:49 +0430

    Delete some extra lines

--- twitcord_Back/twitcord_app/urls.py
+++ twitcord_Back/twitcord_app/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 from allauth.account.views import confirm_email
@@ -7,6 +6,6 @@ from allauth.account.views import confirm_email
 
 urlpatterns = [
     path('tweets/', views.TweetsView.as_view()),
-    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
-        name='account_confirm_email'),
+    re_path('accounts-rest/registration/account-confirm-email/(?P<key>.+)/', confirm_email,
+            name='account_confirm_email')
 ]
