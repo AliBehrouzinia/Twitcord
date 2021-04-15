32d8867d7d95bb80b18c10520d58860e66a30863
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-15 10:46:21 +0430

    Redesign form with Formik

--- twitcord_front/src/Components/EditProfile/EditProfile.js
+++ twitcord_front/src/Components/EditProfile/EditProfile.js
@@ -1,60 +1,115 @@
 /* eslint-disable */
 
 import React from "react";
-import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import "./EditProfile.css";
 import image from "../../assets/image.png";
+import { Formik, Form, Field } from "formik";
+import { Button } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+import { DatePicker } from "formik-material-ui-pickers";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import Box from "@material-ui/core/Box";
 
 const EditProfile = () => {
   return (
-    <Grid container direction="column" className="root-container">
+    <Grid container direction="column">
       <Grid item className="grid-item" xs={12} sm={10} md={8}>
-            <img src={image} alt="img" className="profile_cover" />
-          <Avatar className="avatar" />
+        <img src={image} alt="img" className="profile_cover" />
+        <Avatar className="avatar" />
       </Grid>
 
       <Grid container>
         <Grid item xs={1} sm={2} md={3} lg={4} />
 
         <Grid item xs={10} sm={8} md={6} lg={4}>
-          <form autoComplete="off" className="form">
-            <TextField
-              className="text-field"
-              label="User Name"
-              disabled
-              value="@alibehroozi"
-              variant="outlined"
-            />
-            <TextField
-              id="outlined-basic"
-              className="text-field"
-              label="Name"
-              variant="outlined"
-            />
-            <TextField
-              id="outlined-basic"
-              className="text-field"
-              label="BirthDay"
-              variant="outlined"
-            />
-            <TextField
-              id="outlined-basic"
-              className="text-field"
-              label="Website"
-              variant="outlined"
-            />
-            <TextField
-              id="outlined-basic"
-              className="text-field"
-              label="Bio"
-              variant="outlined"
-              multiline
-              rows={4}
-              rowsMax={8}
-            />
-          </form>
+          <Formik
+            initialValues={{
+              username: "",
+              name: "",
+              website: "",
+              bio: "",
+              date: null,
+            }}
+            onSubmit={(values, {setSubmitting}) => {
+                setSubmitting(false);
+                alert(JSON.stringify(values, null, 2));
+
+            }}
+          >
+            {({ submitForm, isSubmitting }) => (
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Form>
+                  <Box margin={1}>
+                    <Field
+                      component={TextField}
+                      className="text-field"
+                      label="User Name"
+                      value="@alibehrooz"
+                      disabled
+                      variant="outlined"
+                      name="username"
+                    />
+                  </Box>
+
+                  <Box margin={1}>
+                    <Field
+                      component={TextField}
+                      className="text-field"
+                      label="Name"
+                      variant="outlined"
+                      name="name"
+                    />
+                  </Box>
+
+                  <Box margin={1}>
+                    <Field
+                      component={DatePicker}
+                      className="text-field"
+                      variant="outlined"
+                      name="date"
+                      label="Birrth Day"
+                      maxDate={new Date()}
+                    />
+                  </Box>
+
+                  <Box margin={1}>
+                    <Field
+                      component={TextField}
+                      className="text-field"
+                      label="Website"
+                      variant="outlined"
+                      name="website"
+                    />
+                  </Box>
+
+                  <Box margin={1}>
+                    <Field
+                      component={TextField}
+                      className="text-field"
+                      label="Bio"
+                      variant="outlined"
+                      name="bio"
+                    />
+                  </Box>
+
+                  <Box margin={1}>
+                    <Button
+                      variant="contained"
+                      className="text-field"
+                      color="primary"
+                      disabled={isSubmitting}
+                      onClick={submitForm}
+                    >
+                      Submit
+                    </Button>
+                  </Box>
+                </Form>
+              </MuiPickersUtilsProvider>
+            )}
+          </Formik>
         </Grid>
         <Grid item xs={1} sm={2} md={3} lg={4} />
       </Grid>
