165d016bcc1159a832c7ad2b22f1255391d09a10
eatmore18 <reza_alidoost@hotmail.com>
2021-04-17 00:32:51 +0430

    Make the number of the characters standard

--- twitcord_Back/twitcord_app/models.py
+++ twitcord_Back/twitcord_app/models.py
@@ -14,11 +14,11 @@ class TwitcordUser(AbstractBaseUser, PermissionsMixin):
     is_public = models.BooleanField(default=True)
     profile_img = models.ImageField(default='profiles/defaults/user-profile-image.jpg', upload_to='profiles', null=True)
     average_rate = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
-    username = models.TextField(max_length=50, null=True, blank=True)
+    username = models.TextField(max_length= 15, null=True, blank=True)
     is_admin = True
-    first_name = models.TextField(null = True, max_length = 50)
+    first_name = models.CharField(null = True, max_length = 50)
     last_name = models.CharField(null = True, max_length = 50)
-    bio = models.TextField(null = True, max_length = 76)
+    bio = models.TextField(null = True, max_length = 160)
     birth_date = models.DateTimeField(null = True)
     website = models.URLField(null = True)
 
