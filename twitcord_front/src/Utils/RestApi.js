eaf130e5f8e0283d3f8fc4efbced5135c097f182
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-16 17:33:49 +0430

    Reformat RestApi with eslint

--- twitcord_front/src/Utils/RestApi.js
+++ twitcord_front/src/Utils/RestApi.js
@@ -1,22 +1,20 @@
-/* eslint-disable */
-
-import axios from "axios";
-import * as Constants from "./Constants";
+import axios from 'axios';
+import * as Constants from './Constants';
 
 const instance = axios.create({
   baseURL: Constants.BASE_URL,
-  responseType: "json",
+  responseType: 'json',
   headers: {
-    Authorization: "token 3c2380f638b1a5456efe4621a5ff3debac83e457",
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    'Authorization': 'token 3c2380f638b1a5456efe4621a5ff3debac83e457',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
   },
 });
 
 const postTweet = (data) =>
   instance({
-    method: "POST",
-    url: "/tweets/",
+    method: 'POST',
+    url: '/tweets/',
     data,
   });
 
