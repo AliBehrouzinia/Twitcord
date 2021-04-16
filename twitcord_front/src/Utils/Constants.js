d275473e2f45199ff11d8598e4ea119e7edf905d
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-16 17:44:35 +0430

    Add request types to constants

--- twitcord_front/src/Utils/Constants.js
+++ twitcord_front/src/Utils/Constants.js
@@ -3,3 +3,8 @@ export const TWEET_BOX_ROW_MAX = 16;
 export const TWEET_CHAR_LIMIT = 140;
 
 export const BASE_URL = 'http://127.0.0.1:8000';
+
+// request types
+export const POST_REQUEST_METHOD = 'POST';
+export const GET_REQUEST_METHOD = 'GET';
+
