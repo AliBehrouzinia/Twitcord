ba140b6e59861b724ff2ecf6b1f86298b1eb0496
mohammad <gamerplus7831@yahoo.com>
2021-04-19 21:55:43 +0430

    Add serializers of follow api

--- twitcord_Back/twitcord_app/serializers.py
+++ twitcord_Back/twitcord_app/serializers.py
@@ -33,3 +33,56 @@ class TweetSerializer(serializers.ModelSerializer):
     def to_internal_value(self, data):
         data['user'] = self.context['request'].user.id
         return super().to_internal_value(data)
+
+
+class FollowingRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FollowRequest
+        fields = '__all__'
+
+
+class FollowersRequestsSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        result = super(FollowersRequestsSerializer, self).to_representation(instance)
+        from_user = instance.request_from
+        result['profile_img'] = from_user.profile_img.url
+        result['username'] = from_user.email
+        return result
+
+    class Meta:
+        model = FollowRequest
+        fields = ['id', 'request_from', 'date']
+
+
+class FollowingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollowing
+        fields = '__all__'
+
+
+class ListOfFollowingsSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        result = super(ListOfFollowingsSerializer, self).to_representation(instance)
+        user = instance.following_user_id
+        result['id'] = result.pop('following_user_id')
+        result['profile_img'] = user.profile_img.url
+        result['username'] = user.email
+        return result
+
+    class Meta:
+        model = UserFollowing
+        fields = ['following_user_id']
+
+
+class ListOfFollowersSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        result = super(ListOfFollowersSerializer, self).to_representation(instance)
+        user = instance.user_id
+        result['id'] = result.pop('user_id')
+        result['profile_img'] = user.profile_img.url
+        result['username'] = user.email
+        return result
+
+    class Meta:
+        model = UserFollowing
+        fields = ['user_id']
