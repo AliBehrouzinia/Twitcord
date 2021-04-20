5473091da562add4ca81184d081b49ac300d3b90
hana-kr <hanakariman@yahoo.com>
2021-04-21 00:43:47 +0430

    rename file

--- twitcord_front/src/App.js
+++ twitcord_front/src/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './redux/store.js';
 // import {BrowserRouter, Route} from 'react-router-dom';
-import Profile from './Components/Profile/Profile.js';
+import Profile from './Components/Profile/profile.js';
 function App() {
   return (
     <Provider store={store}>
