9592fcbee2d183706644d6510bf38f5e8ddf9566
ali behroozi <ali.bhrzinia@gmail.com>
2021-04-27 00:51:36 +0430

    Add edit profile test file

---
+++ twitcord_front/src/Components/EditProfile/EditProfile.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditProfile from './EditProfile';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import {configure} from 'enzyme';
+
+
+configure({adapter: new Adapter()});
+
+describe('EditProfile', () => {
+  
+});
+
