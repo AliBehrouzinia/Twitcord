568409ae29df5bad93933b7ec654bb2036e2d9aa
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-01 23:59:32 +0430

    Add search component ui

---
+++ twitcord_front/src/Components/Search/Search.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Paper from '@material-ui/core/Paper';
+import {SearchBar} from '../SearchBar/SearchBar';
+
+
+export const Search = () => {
+  const [tabSelected, setSelectedTab] = React.useState(0);
+
+  const handleChange = (event, selectedTab) => {
+    setSelectedTab(selectedTab);
+  };
+
+  return (
+    <div>
+      <Paper square>
+        <SearchBar/>
+        <Tabs
+          variant="fullWidth"
+          value={tabSelected}
+          indicatorColor="primary"
+          onChange={handleChange}
+          textColor="primary"
+          aria-label="disabled tabs example"
+        >
+          <Tab label="users" />
+          <Tab label="tweets" />
+        </Tabs>
+      </Paper>
+    </div>
+  );
+};
--- twitcord_front/src/Components/SearchBar/SearchBar.js
+++ twitcord_front/src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
-import Paper from '@material-ui/core/Paper';
 import SearchIcon from '@material-ui/icons/Search';
 import './SearchBar.css';
 
 export const SearchBar = () => {
   return (
-    <Paper
+    <div
       component="form"
       className="root">
       <InputBase
@@ -19,5 +18,5 @@ export const SearchBar = () => {
         aria-label="search">
         <SearchIcon />
       </IconButton>
-    </Paper> );
+    </div> );
 };
