4079a828d233d3116fd89e59c0856453a6b8a0f9
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-06 13:16:21 +0430

    Improve profile ui

--- twitcord_front/src/Components/Profile/Profile.js
+++ twitcord_front/src/Components/Profile/Profile.js
@@ -2,34 +2,39 @@ import React from 'react';
 import ProfileUserinfo from '../ProfileUserinfo/ProfileUserinfo';
 import ProfileTweetlist from '../ProfileTweetlist/ProfileTweetlist';
 import './Profile.css';
-import {useState} from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 const Profile = () => {
-  const [componentid, setcomponentid] = useState('');
+  const [tabSelected, setSelectedTab] = React.useState(0);
+
+  const handleChange = (event, selectedTab) => {
+    setSelectedTab(selectedTab);
+  };
+
   return (
     <Grid container item className="grid-item">
-      <Grid  xs={12} sm={10} md={8}>
+      <Grid  xs={12}  md={8}>
         <ProfileUserinfo/>
-        <div className= "info3">
-          <button className="button-tweet"
-            id='1' onClick={() => setcomponentid(1)}>tweets</button>
-          <button className="button"
-            id='2'onClick={() => setcomponentid(2)}>replys</button>
-          <button className="button"
-            id='3' onClick={() => setcomponentid(3)}>likes</button>
-          <button className="button-rooms"
-            id='4' onClick={() => setcomponentid(4)}>rooms</button>
-        </div>
+        <Tabs 
+        variant="fullWidth"
+        value={tabSelected} 
+        onChange={handleChange} 
+        indicatorColor="primary"
+        textColor="primary">
+          <Tab label="tweets"  />
+          <Tab label="replys"  />
+          <Tab label="likes" />
+          <Tab label="rooms"  />
+        </Tabs>
       </Grid>
-      <Grid  xs={12} sm={10} md={8}>
-        {componentid === 1 ? (
-            <ProfileTweetlist/>
-          ) : (
-            <div className = "choose">
-              <div > choose a tab</div>
-            </div>
-          )}
+      <Grid  xs={12}  md={8}>
+      {tabSelected == 0 && <p className="profile-content">tweets</p> }
+      {tabSelected == 1 && <p className="profile-content">replys</p> }
+      {tabSelected == 2 && <p className="profile-content">likes</p> }
+      {tabSelected == 3 && <p className="profile-content">rooms</p> }
       </Grid>
     </Grid>
   );
