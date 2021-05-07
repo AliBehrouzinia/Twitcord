4b552da1bf4ab9ce1c6476b2b2c0c18cef34624f
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-08 01:15:17 +0430

    Add home page base

---
+++ twitcord_front/src/Components/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import  SideDrawer from '../HomePage/SideDrawer/SideDrawer.js';
+import './HomePage.css'
+import Typography from '@material-ui/core/Typography';
+
+
+const HomePage = () => {
+
+    return(
+        <Grid container className="hp-container">
+            <Grid item xs={3} className="hp-side-left">
+                <SideDrawer />
+            </Grid>
+            <Grid item xs>                 
+            </Grid>
+            <Grid item xs={12} md={3} className="hp-side-right">
+                <Typography className="hp-suggestion">Suggestions</Typography>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default HomePage;
