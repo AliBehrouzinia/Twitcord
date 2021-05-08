87a81d3603b4089c2e3de6dc1e967386fe4651e8
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-08 20:38:12 +0430

    Reformat components with eslint

--- twitcord_front/src/Components/HomePage/HomePage.js
+++ twitcord_front/src/Components/HomePage/HomePage.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import  SideDrawer from '../HomePage/SideDrawer/SideDrawer.js';
-import './HomePage.css'
+import SideDrawer from '../HomePage/SideDrawer/SideDrawer.js';
+import './HomePage.css';
 import Typography from '@material-ui/core/Typography';
-import TweetBox from '../TweetBox/TweetBox'
-import { Divider } from '@material-ui/core';
+import TweetBox from '../TweetBox/TweetBox';
+import {Divider} from '@material-ui/core';
 
 const HomePage = () => {
-
-    return(
-        <Grid container className="hp-container">
-            <Grid item xs={4} className="hp-side-left">
-                <SideDrawer />
-            </Grid>
-            <Grid item xs>
-                <TweetBox/>    
-                <Divider/>     
-            </Grid>
-            <Grid item xs={12} md={4} className="hp-side-right">
-                <Typography className="hp-suggestion">Suggestions</Typography>
-            </Grid>
-        </Grid>
-    )
-}
+  return (
+    <Grid container className="hp-container">
+      <Grid item xs={4} className="hp-side-left">
+        <SideDrawer />
+      </Grid>
+      <Grid item xs>
+        <TweetBox/>
+        <Divider/>
+      </Grid>
+      <Grid item xs={12} md={4} className="hp-side-right">
+        <Typography className="hp-suggestion">Suggestions</Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default HomePage;
--- twitcord_front/src/Components/HomePage/SideDrawer/SideDrawer.js
+++ twitcord_front/src/Components/HomePage/SideDrawer/SideDrawer.js
@@ -1,100 +1,101 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
-import RecordVoiceOverRoundedIcon from '@material-ui/icons/RecordVoiceOverRounded';
+import RecordVoiceOverRoundedIcon from
+  '@material-ui/icons/RecordVoiceOverRounded';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import Typography from '@material-ui/core/Typography';
-import './SideDrawer.css'
+import './SideDrawer.css';
 import {useSelector} from 'react-redux';
 import {useDispatch} from 'react-redux';
 import * as Actions from '../../../redux/Actions/index';
 
 
 const SideDrawer = () => {
-  const windowHeight = window['innerHeight']
+  const windowHeight = window['innerHeight'];
   const selectedIndex = useSelector((state) => state).tweet.drawerSelectedTab;
   const dispatch = useDispatch();
 
   return (
-      <div style={{height:windowHeight}}>
-        <div className="sd-container">
-            <Typography className="app-name">TweetCord</Typography>
-            <div 
-            onClick={() => dispatch(
+    <div style={{height: windowHeight}}>
+      <div className="sd-container">
+        <Typography className="app-name">TweetCord</Typography>
+        <div
+          onClick={() => dispatch(
               Actions.setDrawerSelectedTab({
                 selectedTab: 0,
               }),
-            )}
-            className={selectedIndex === 0 ? "sd-item-selected" : "sd-item"}
-            >
-              <HomeRoundedIcon className="sd-icon" />
-              <Typography className="sd-title">home</Typography>
-            </div>
+          )}
+          className={selectedIndex === 0 ? 'sd-item-selected' : 'sd-item'}
+        >
+          <HomeRoundedIcon className="sd-icon" />
+          <Typography className="sd-title">home</Typography>
+        </div>
 
-            <div 
-            onClick={() => dispatch(
+        <div
+          onClick={() => dispatch(
               Actions.setDrawerSelectedTab({
                 selectedTab: 1,
               }),
-            )}
-            className={selectedIndex === 1 ? "sd-item-selected" : "sd-item"}
-            >
-              <PersonRoundedIcon className="sd-icon" />
-              <Typography className="sd-title">Profile</Typography>
-            </div>
+          )}
+          className={selectedIndex === 1 ? 'sd-item-selected' : 'sd-item'}
+        >
+          <PersonRoundedIcon className="sd-icon" />
+          <Typography className="sd-title">Profile</Typography>
+        </div>
 
-            <div
-            onClick={() => dispatch(
+        <div
+          onClick={() => dispatch(
               Actions.setDrawerSelectedTab({
                 selectedTab: 2,
               }),
-            )}
-            className={selectedIndex === 2 ? "sd-item-selected" : "sd-item"}
-            >
-              <NotificationsRoundedIcon className="sd-icon" />
-              <Typography className="sd-title">Notification</Typography>
-            </div>
+          )}
+          className={selectedIndex === 2 ? 'sd-item-selected' : 'sd-item'}
+        >
+          <NotificationsRoundedIcon className="sd-icon" />
+          <Typography className="sd-title">Notification</Typography>
+        </div>
 
-            <div
-            onClick={() => dispatch(
+        <div
+          onClick={() => dispatch(
               Actions.setDrawerSelectedTab({
                 selectedTab: 3,
               }),
-            )}
-            className={selectedIndex === 3 ? "sd-item-selected" : "sd-item"}
-            >
-              <SearchRoundedIcon className="sd-icon"/>
-              <Typography className="sd-title">Search</Typography>
-            </div>
+          )}
+          className={selectedIndex === 3 ? 'sd-item-selected' : 'sd-item'}
+        >
+          <SearchRoundedIcon className="sd-icon"/>
+          <Typography className="sd-title">Search</Typography>
+        </div>
 
-            <div 
-            onClick={() => dispatch(
+        <div
+          onClick={() => dispatch(
               Actions.setDrawerSelectedTab({
                 selectedTab: 4,
               }),
-            )}
-            className={selectedIndex === 4 ? "sd-item-selected" : "sd-item"}
-            >
-              <RecordVoiceOverRoundedIcon className="sd-icon"/>
-              <Typography className="sd-title">Room</Typography>
-            </div>
+          )}
+          className={selectedIndex === 4 ? 'sd-item-selected' : 'sd-item'}
+        >
+          <RecordVoiceOverRoundedIcon className="sd-icon"/>
+          <Typography className="sd-title">Room</Typography>
+        </div>
 
-            <div 
-            onClick={() => dispatch(
+        <div
+          onClick={() => dispatch(
               Actions.setDrawerSelectedTab({
                 selectedTab: 5,
               }),
-            )}
-            className={selectedIndex === 5 ? "sd-item-selected" : "sd-item"}
-            >
-              <MailOutlineIcon className="sd-icon"/>
-              <Typography className="sd-title">Message</Typography>
-            </div>
+          )}
+          className={selectedIndex === 5 ? 'sd-item-selected' : 'sd-item'}
+        >
+          <MailOutlineIcon className="sd-icon"/>
+          <Typography className="sd-title">Message</Typography>
         </div>
       </div>
+    </div>
   );
-}
+};
 
 export default SideDrawer;
