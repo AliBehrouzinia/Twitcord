c71f063ee796f96b09388f5ac748c06ab815da6e
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-08 22:42:49 +0430

    Move Side drawer from homepage to app

--- twitcord_front/src/App.js
+++ twitcord_front/src/App.js
@@ -7,23 +7,42 @@ import {Provider} from 'react-redux';
 import store from './redux/store.js';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Profile from './Components/Profile/Profile.js';
+import Search from './Components/Search/Search.js';
+import Grid from '@material-ui/core/Grid';
+import EditProfile from './Components/EditProfile/EditProfile.js';
+import SideDrawer from './Components/HomePage/SideDrawer/SideDrawer.js';
 
 function App() {
+  const windowHeight = window['innerHeight'];
+
   return (
     <Provider store={store}>
-      <div className="App">
-        {/* <SignUp></SignUp>
-        <TweetBox /> */}
-        <BrowserRouter>
-          <div>
-            <Route exact path="/hp" component={HomePage} />
-            <Route exact path="/edit-profile" component={EditProfile} />
-            <Route exact path="/login" component={LogIn} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/profile" component={Profile} />
-          </div>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Grid container className="hp-container">
+          <Grid item xs={4} className="hp-side-left">
+            <SideDrawer />
+          </Grid>
+          <Grid item xs>
+            <div className="App">
+              <div>
+                <Route exact path="/search" component={Search} />
+                <Route exact path="/homepage" component={HomePage} />
+                <Route exact path="/login" component={LogIn} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route exact path="/profile" component={Profile} />
+                <Route exact path="/notification" component={EditProfile} />
+              </div>
+            </div>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            lg={3}
+            style={{height: windowHeight}}
+            className="hp-side-right">
+          </Grid>
+        </Grid>
+      </BrowserRouter>
     </Provider>
   );
 }
--- twitcord_front/src/Components/HomePage/HomePage.js
+++ twitcord_front/src/Components/HomePage/HomePage.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import SideDrawer from '../HomePage/SideDrawer/SideDrawer.js';
 import './HomePage.css';
-import Typography from '@material-ui/core/Typography';
 import TweetBox from '../TweetBox/TweetBox';
 import {Divider} from '@material-ui/core';
+
 
 const HomePage = () => {
   return (
     <Grid container className="hp-container">
-      <Grid item xs={4} className="hp-side-left">
-        <SideDrawer />
-      </Grid>
       <Grid item xs>
         <TweetBox/>
         <Divider/>
-      </Grid>
-      <Grid item xs={12} md={4} className="hp-side-right">
-        <Typography className="hp-suggestion">Suggestions</Typography>
       </Grid>
     </Grid>
   );
