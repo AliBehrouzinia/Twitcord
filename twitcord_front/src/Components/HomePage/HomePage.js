cd9ca15764845030bcd03eb41642dbda80d5569b
ali behroozi <ali.bhrzinia@gmail.com>
2021-05-08 20:34:44 +0430

    Add tweet box to homepage

--- twitcord_front/src/Components/HomePage/HomePage.js
+++ twitcord_front/src/Components/HomePage/HomePage.js
@@ -4,18 +4,20 @@ import  SideDrawer from '../HomePage/SideDrawer/SideDrawer.js';
 import './HomePage.css'
 import Typography from '@material-ui/core/Typography';
 import TweetBox from '../TweetBox/TweetBox'
+import { Divider } from '@material-ui/core';
 
 const HomePage = () => {
 
     return(
         <Grid container className="hp-container">
-            <Grid item xs={3} className="hp-side-left">
+            <Grid item xs={4} className="hp-side-left">
                 <SideDrawer />
             </Grid>
             <Grid item xs>
-                                
+                <TweetBox/>    
+                <Divider/>     
             </Grid>
-            <Grid item xs={12} md={3} className="hp-side-right">
+            <Grid item xs={12} md={4} className="hp-side-right">
                 <Typography className="hp-suggestion">Suggestions</Typography>
             </Grid>
         </Grid>
