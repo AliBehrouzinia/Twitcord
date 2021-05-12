caa6733e3b7dbe62288c1786816efb9e9e5ce4e7
aidashr <45814380+aidashr@users.noreply.github.com>
2021-05-12 19:24:27 +0430

    tweetlist

--- twitcord_front/src/Components/ProfileTweetlist/ProfileTweetlist.js
+++ twitcord_front/src/Components/ProfileTweetlist/ProfileTweetlist.js
@@ -3,18 +3,14 @@ import React, { useEffect, useState } from 'react';
 import Tweets from '../Tweets/Tweets';
 import Grid from '@material-ui/core/Grid';
 import './ProfileTweetlist.css';
-import { useDispatch } from 'react-redux';
 import Divider from '@material-ui/core/Divider';
-import { useSelector } from 'react-redux';
 import * as API from '../../Utils/API/index';
-import * as Actions from '../../redux/Actions/index.js';
 import * as Constants from '../../Utils/Constants.js';
-import axios from "axios";
+
 
 const ProfileTweetlist = (props) => {
-    // const tweets = useSelector((state) => state).tweet.tweetInfo;
     const [tweets, setTweets] = useState([]);
-    // const dispatch = useDispatch();
+
     let profileId = -1;
     const userGeneralInfo = JSON.parse(
         localStorage.getItem(Constants.GENERAL_USER_INFO),
@@ -23,31 +19,21 @@ const ProfileTweetlist = (props) => {
         profileId = userGeneralInfo.pk;
     }
     useEffect(() => {
-        tweetlist(profileId)
-        // Run! Like go get some data from an API.
-    });
-    const tweetlist = (id) => {
-
-        // API.tweetlist({ id: profileid })
-        //     .then((response) => {
-        //         const data = response.data;
-        //         dispatch(Actions.setTweetListInfo(response.data));
-        //     })
-        //     .catch((error) => {
-        //     });
 
         console.log(tweets)
 
-        axios.get("http://127.0.0.1:8000" + `/tweets/${id}/`).then(data => {
-            console.log(data)
-            setTweets(data.data.results)
-        }
+        API.tweetlist({ id: profileId })
+            .then((data) => {
+                console.log(data)
+                setTweets(data.data.results)
+            })
+            .catch((error) => {
+                console.log('failed to load data');
+            });
+
+    });
 
 
-
-
-        )
-    };
     return (
         <div>
             <Grid container item className="tweetlist">
