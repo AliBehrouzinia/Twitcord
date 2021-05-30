dfb32ba53bdf0b7ab63893d43e5085c0ad87abf7
mohammadym <gamerplus7831@yahoo.com>
2021-05-30 17:01:06 +0430

    Add is_liked filed in TweetSearch api

--- twitcord_Back/twitcord_app/serializers.py
+++ twitcord_Back/twitcord_app/serializers.py
@@ -176,6 +176,18 @@ class GlobalTweetSearchSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         result = super(GlobalTweetSearchSerializer, self).to_representation(instance)
         user = instance.user
+        request_user = self.context['request'].user.id
+        likes = Like.objects.filter(user=request_user)
+        liked_tweets = []
+        for item in likes:
+            temp = Tweet.objects.filter(id=item.tweet.id)
+            liked_tweets.append(temp)
+        for item in liked_tweets:
+            if instance == item[0]:
+                result['is_liked'] = True
+                break
+        else:
+            result['is_liked'] = False
         result['id'] = result.pop('user')
         result['profile_img'] = user.profile_img.url
         result['username'] = user.username
