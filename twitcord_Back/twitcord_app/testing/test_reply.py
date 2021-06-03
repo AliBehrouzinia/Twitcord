13fcbf70db0e289ce09a9932ca40248dbbe27d57
mohammadym <gamerplus7831@yahoo.com>
2021-06-03 12:59:11 +0430

    Finish test of reply and list of replies

--- twitcord_Back/twitcord_app/testing/test_reply.py
+++ twitcord_Back/twitcord_app/testing/test_reply.py
@@ -8,7 +8,7 @@ from ..models import Tweet, Like
 from rest_framework import serializers
 
 
-class LikeTest(APITestCase):
+class ReplyTest(APITestCase):
     """
     Ensure we can get list of followings.
     Ensure we can unfollow a user.
@@ -25,14 +25,16 @@ class LikeTest(APITestCase):
         self.user2 = twitcord_user.objects.create(username='test2', email='test2@gmail.com', password='testpass')
         self.user3 = twitcord_user.objects.create(username='test3', email='test3@gmail.com', password='testpass')
 
-        url = '/reply/'
+    def test_reply(self):
 
+        url = '/reply/'
+        self.maxDiff = None
         data = {
             "content": "reply1",
             "parent": self.tweet.id
         }
 
-        response = self.client.post(url, data, content_type='application/json')
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         create_date = serializers.DateTimeField().to_representation(response.data['create_date'])
 
@@ -42,48 +44,29 @@ class LikeTest(APITestCase):
             "parent": 1,
             "content": "reply1",
             "create_date": create_date,
+            "retweet_from": None,
             "user": 1
         }
-
         self.assertEqual(response.data, response_data)
 
-        self.reply2 = models.Tweet.objects.create(user=self.user1, content='reply2', parent=self.tweet.id)
-        self.reply3 = models.Tweet.objects.create(user=self.user2, content='reply3', parent=self.tweet.id)
-
-        self.assertEqual(len(models.Tweet.objects.filter(parent=self.tweet.id)), 3)
+        self.assertEqual(len(models.Tweet.objects.filter(parent=self.tweet.id)), 1)
 
         url = '/replys/{}/'.format(self.tweet.id)
         response = self.client.get(url)
         data = {
-            "count": 3,
+            "count": 1,
             "next": None,
             "previous": None,
-            "results": [
+            "results":
                 {
                     "id": 2,
                     "is_reply": True,
                     "parent": 1,
                     "content": "reply1",
                     "create_date": create_date,
-                    "user": self.user
-                },
-                {
-                    "id": 3,
-                    "is_reply": True,
-                    "parent": 1,
-                    "content": "reply2",
-                    "create_date": serializers.DateTimeField().to_representation(self.reply2.create_date),
-                    "user": self.user1.id
-                },
-                {
-                    "id": 4,
-                    "is_reply": True,
-                    "parent": 1,
-                    "content": "reply3",
-                    "create_date": serializers.DateTimeField().to_representation(self.reply3.create_date),
-                    "user": self.user2.id
+                    "retweet_from": None,
+                    "user": self.user.id
                 }
-            ]
         }
 
         result = response.data
