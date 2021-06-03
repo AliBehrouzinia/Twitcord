9c05f7837294e8e5941c28c5fbf0ffd7ead457da
mohammadym <gamerplus7831@yahoo.com>
2021-06-03 21:32:33 +0430

    Add retweet and reply count to timeline response

--- twitcord_Back/twitcord_app/serializers.py
+++ twitcord_Back/twitcord_app/serializers.py
@@ -256,6 +256,8 @@ class TimeLineSerializer(serializers.ModelSerializer):
         result['last_name'] = user.last_name
         result['is_public'] = user.is_public
         result['like_count'] = len(Like.objects.filter(tweet_id=instance.id))
+        result['reply_count'] = len(Tweet.objects.filter(parent_id=instance.id))
+        result['retweet_count'] = len(Tweet.objects.filter(retweet_from_id=instance.id))
         return result
 
 
